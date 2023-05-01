9b8551da777fd2458a1d1cb6272429d08b5b98e7
Joey Yandle <xoloki@gmail.com>
2023-05-01 19:24:45 -0400

    get docs rebranded and pointing correctly

--- .doc/build.sh
+++ .doc/build.sh
@@ -4,11 +4,11 @@ cargo doc --no-deps
 # We have to build the pdf twice. The way the bibliography works is that the
 # first run creates the bibtex aux files, then the second run puts the ref
 # numbers into the inline citations.
-pdflatex ./wtf.tex
-pdflatex ./wtf.tex
+pdflatex ./wsts.tex
+pdflatex ./wsts.tex
 
 # Copy PDF File
-cp ./wtf.pdf ./target/doc/
+cp ./wsts.pdf ./target/doc/
 
 # Copy index.html
 cp ./.doc/index.html ./target/doc/
